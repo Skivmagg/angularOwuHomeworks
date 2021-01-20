75a6ce8342d33ba2ce58a75700f8e59d340fbaa9
AntonBuchynskyi <skivmagg@gmail.com>
2021-01-20 18:44:54 +0200

    First homework done

---
+++ src/app/data/cars.ts
@@ -0,0 +1,7 @@
+export const cars = [
+  {model: 'Tesla', year: 2010, power: 430, color: 'red'},
+  {model: 'BMW', year: 2015, power: 177, color: 'blue'},
+  {model: 'OPEL', year: 2012, power: 120, color: 'red'},
+  {model: 'OPEL', year: 2012, power: 120, color: 'red'},
+  {model: 'JEEP', year: 2013, power: 300, color: 'yellow'},
+  {model: 'Toyota', year: 2020, power: 400, color: 'red'}, ];

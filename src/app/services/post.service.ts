3fb2d55bb45a07d689e4579786db3559b454d160
AntonBuchynskyi <skivmagg@gmail.com>
2021-01-21 21:49:17 +0200

    Posts done

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Posts} from '../../models/Posts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private httpClient:HttpClient) { }
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+
+  getPosts(){
+    return this.httpClient.get<Posts[]>(this.url)
+  }
+
+
+}
